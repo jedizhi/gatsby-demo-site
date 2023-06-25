eeecf6569ed142b385717aca534c118a6092bd8f
Jedizhi <jedizhi@gmail.com>
2023-06-25 11:34:25 +0300

    first commit

---
+++ src/pages/my-componet.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+// define component
+
+const MyComponent = () => {
+
+    return (
+        <h1>Hello, Welcom to Gatsby Demo Site</h1>
+    )
+}
+
+// export component
+export default MyComponent
