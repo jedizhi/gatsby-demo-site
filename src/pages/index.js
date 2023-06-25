48d6e678a31098be128e02c6b090d9defe4cdd05
Jedizhi <jedizhi@gmail.com>
2023-06-25 15:36:21 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
+
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
       <Layout pageTitle={"Home Page"}>
       <p>This is an awesome Gatsby App.</p>
+      <StaticImage
+        //alt = "Mochi and Snowy , two cute and playfull pomerians"
+        //src = "https://images.pexels.com/photos/4262325/pexels-photo-4262325.jpeg?auto=compress&cs=tinysrgb&w=600"
+        alt = "Snowy , cute and playfull pomerians"
+        src = "../images/snowy.jpg"
+      
+      />      
       </Layout>
   )
 }
